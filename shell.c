a4184c8ddab83a1df573a4e245a4d3678505d28d
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-03-15 20:24:18 +0200

    feat shell_loop()

--- shell.c
+++ shell.c
@@ -1,5 +1,30 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char* shell_read();
+
+char** shell_line_split(char*);
+
+int shell_execute(char**);
 
 int main(int argc, char* argv){
-    
+    shell_loop();
+
+    return EXIT_SUCCESS;
+}
+
+void shell_loop(){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("> ");
+        line = shell_read();
+        args = shell_line_split(line);
+        status = shell_execute(args);
+
+        free(line);
+        free(args);
+    } while(status);
 }
--- shell.h
+++ shell.h
@@ -2,3 +2,25 @@
 #include <stdlib.h>
 
 int main(int, char*);
+
+void shell_loop(){
+    char *line;
+    char **args;
+    int status;
+
+    do{
+        printf("> ");
+        line = shell_read();
+        args = shell_line_split(line);
+        status = shell_execute(args);
+
+        free(line);
+        free(args);
+    } while(status);
+}
+
+char* shell_read();
+
+char** shell_line_split(char*);
+
+int shell_execute(char**);
