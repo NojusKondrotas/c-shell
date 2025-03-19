a3053f2d19a313ebd81d25dc5f2be88d3e2d712b
NojusKondrotas <nojus.kondrotas23@gmail.com>
2025-03-19 20:52:49 +0200

    feat launching program through shell

--- shell.c
+++ shell.c
@@ -100,3 +100,25 @@ char **shell_line_split(char* line){
 int shell_execute(char** args){
     return 1;
 }
+
+int shell_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if(pid == 0){
+        if(execvp(args[0], args) == -1)
+            perror("shell");
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0){
+        perror("shell");
+    }
+    else{
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
